b4e0cdd473bf74feffaa3af54e4942f42aabb300
Armand Mégrot <armand.megrot@gmail.com>
2023-07-07 21:22:38 +0000

    Implem forward tcpip request

--- src/ssh/mod.rs
+++ src/ssh/mod.rs
@@ -1,16 +1,20 @@
 use anyhow::{Context, Ok, Result};
 use async_trait::async_trait;
 use derive_more::Constructor;
-use log::debug;
-use russh::{client, ChannelId};
+use russh::{
+    client::{self, Msg, Session},
+    Channel,
+};
 use russh_keys::key;
 use std::sync::Arc;
+use tokio::net::TcpStream;
 use tokio_util::sync::CancellationToken;
 
 use crate::{connection::Connection, server_conf, Options};
 
 #[derive(Constructor)]
 pub struct Client {
+    options: Options,
     server_conf: server_conf::Conf,
 }
 
@@ -21,7 +25,6 @@ pub async fn start(
     cancellation_token: CancellationToken,
 ) -> Result<()> {
     let config = russh::client::Config::default();
-    let config = Arc::new(config);
 
     let host = options
         .host
@@ -30,24 +33,20 @@ pub async fn start(
         .context("Could not get host part")?
         .to_string();
     let addr = (host, server_conf.sshd_port.parse()?);
-    let mut session = russh::client::connect(config, addr, Client { server_conf }).await?;
+    let mut session = russh::client::connect(Arc::new(config), addr, Client { options: options.clone(), server_conf }).await?;
 
     if session
         .authenticate_password("expose", options.secret.clone())
         .await?
     {
-        let mut channel = session.channel_open_session().await?;
-        channel.data(&b"Hello, world!"[..]).await?;
-        loop {
-            tokio::select! {
-                msg = channel.wait() => {
-                    debug!("{msg:?}");
-                },
-                _ = cancellation_token.cancelled() => {
-                    return Ok(())
-                }
-            }
-        }
+        let forward_addr = format!("{}.{}", options.subdomain, options.host);
+        tokio::select! {
+            res = session.tcpip_forward(forward_addr.clone(), 0) => {
+                res?;
+            },
+            _ = cancellation_token.cancelled() => {}
+        };
+        session.cancel_tcpip_forward(forward_addr, 0).await?;
     }
     Ok(())
 }
@@ -64,17 +63,18 @@ impl client::Handler for Client {
         Ok((self, matches))
     }
 
-    async fn data(
+    async fn server_channel_open_forwarded_tcpip(
         self,
-        channel: ChannelId,
-        data: &[u8],
-        session: client::Session,
-    ) -> Result<(Self, client::Session), Self::Error> {
-        debug!(
-            "data on channel {:?}: {:?}",
-            channel,
-            std::str::from_utf8(data)
-        );
+        channel: Channel<Msg>,
+        _connected_address: &str,
+        _connected_port: u32,
+        _originator_address: &str,
+        _originator_port: u32,
+        session: Session,
+    ) -> Result<(Self, Session), Self::Error> {
+        let mut stream = TcpStream::connect(format!("127.0.0.1:{}", self.options.port)).await?;
+
+        tokio::io::copy_bidirectional(&mut channel.into_stream(), &mut stream).await?;
         Ok((self, session))
     }
 }
